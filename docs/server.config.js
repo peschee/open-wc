43cfc753e6019e4744d1d1827e466d84b38890dd
Thomas Allmer <d4kmor@gmail.com>
2020-05-16 20:14:53 +0200

    chore: use es-dev-server plugin

--- docs/server.config.js
+++ docs/server.config.js
@@ -1,16 +1,52 @@
+const Eleventy = require('@11ty/eleventy');
+
+const elev = new Eleventy('./docs', './__site');
+elev.setConfigPathOverride('./docs/.eleventy.js');
+elev.setDryRun(true); // do not write to file system
+
 module.exports = {
   nodeResolve: true,
-  open: './_site-dev',
-  responseTransformers: [
-    function rewriteBasePath({ contentType, body }) {
-      if (contentType.includes('text/html')) {
-        return {
-          body: body
-            .replace(/href="\//g, 'href="/_site-dev/')
-            .replace(/src="\//g, 'src="/_site-dev/'),
-        };
+  watch: true,
+  open: './docs/README.md',
+  middlewares: [
+    async (ctx, next) => {
+      if (ctx.path.endsWith('index.html')) {
+        ctx.path = ctx.path.replace('index.html', 'README.md');
+      } else if (ctx.path.endsWith('.html')) {
+        ctx.path = ctx.path.replace('.html', '.md');
+      } else if (ctx.path.endsWith('/')) {
+        ctx.path += 'README.md';
       }
-      return null;
+      return next();
+    },
+  ],
+  plugins: [
+    {
+      async transform(context) {
+        if (context.path.endsWith('md')) {
+          const serverPath = context.path;
+          let { body } = 'File not found';
+          elev.config.filters['hook-for-dev-server'] = (content, outputPath, inputPath) => {
+            if (inputPath === `.${serverPath}`) {
+              body = content;
+            }
+            return content;
+          };
+
+          await elev.init();
+          await elev.write();
+          return {
+            body: body.replace(/href="\//g, 'href="/docs/').replace(/src="\//g, 'src="/docs/'),
+          };
+        }
+        return undefined;
+      },
+      resolveMimeType(context) {
+        if (context.path.endsWith('md')) {
+          return 'text/html';
+        }
+        return undefined;
+      },
     },
   ],
 };
